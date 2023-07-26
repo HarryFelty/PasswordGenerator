bd294742c833986c2d0a1d6158b2ad6f0219571c
HarryFelty <harryfelty@gmail.com>
2023-07-26 17:13:56 -0400

    added array of lowercase alphabet

---
+++ script.js
@@ -0,0 +1,21 @@
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
+
+let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// Returns random password
+function generatePassword() {
+
+}
+
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
